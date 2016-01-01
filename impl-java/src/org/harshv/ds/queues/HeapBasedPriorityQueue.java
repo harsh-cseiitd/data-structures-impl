2fceba82f8434583af3492f7e77523512cfff105
harsh-cseiitd <harsh.cseiitd@gmail.com>
2016-01-01 10:50:43 -0800

    added apache license

--- impl-java/src/org/harshv/ds/queues/HeapBasedPriorityQueue.java
+++ impl-java/src/org/harshv/ds/queues/HeapBasedPriorityQueue.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (C) 2016 Harsh Vardhan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.harshv.ds.queues;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 
-public <T extends Comparable<T>> class HeapBasedPriorityQueue<T> implements PriorityQueue<T> {
+public class HeapBasedPriorityQueue<T extends Comparable<T>> implements PriorityQueue<T > {
 	
 	private BinaryHeap<T> bHeap;
 	
@@ -54,6 +70,14 @@ public <T extends Comparable<T>> class HeapBasedPriorityQueue<T> implements Prio
 	}
 
 	public boolean	removeElement(T element) {
+		
+	}
+	
+	void decreasePriority(T element, T newElement) {
+		
+	}
+
+	void increasePriority(T element, T newElement) {
 		
 	}
 
